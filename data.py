fe2973fe3c51a4e78f119d49369ba20cedb07752
Amina-Sagheer <sagheeramina32@gmail.com>
2024-06-27 09:24:55 -0600

    Add completed data file

---
+++ data.py
@@ -0,0 +1,14 @@
+question_data = [
+    {"text": "The capital of Australia is Sydney.", "answer": "False"},
+    {"text": "There are more stars in the universe than grains of sand on Earth.", "answer": "True"},
+    {"text": "Honey never spoils.", "answer": "True"},
+    {"text": "Humans can distinguish over a trillion different smells.", "answer": "True"},
+    {"text": "Bananas grow on trees.", "answer": "False"},
+    {"text": "The Great Wall of China is visible from space with the naked eye.", "answer": "False"},
+    {"text": "Octopuses have three hearts.", "answer": "True"},
+    {"text": "Lightning never strikes the same place twice.", "answer": "False"},
+    {"text": "A sneeze travels about 100 miles per hour.", "answer": "True"},
+    {"text": "Humans and dinosaurs coexisted.", "answer": "False"},
+    {"text": "The tallest mountain on Earth is Mount Everest.", "answer": "True"},
+    {"text": "A single strand of spaghetti is called a spaghetto.", "answer": "True"}
+]
